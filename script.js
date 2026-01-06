279d5052aca061311299f6d10d4ee508ee68b9a3
igor4022 <tic80padraolua@gmail.com>
2026-01-06 18:21:53 -0300

    sim

--- script.js
+++ script.js
@@ -1,13 +1,49 @@
-const val = document.querySelectorAll('p[class^="numero"')
-let i = 1
+const eles = document.querySelector('.eles')
+const nos = document.querySelector('.nos')
+const criaEles = document.createElement('p')
+const criaNos = document.createElement('p')
 
-function adiciona() {
+const titulo = document.querySelector('.titulo')
+const texto = document.createElement('p')
+const venci = document.querySelectorAll('.vencedor')
 
-    val.innerHTML = ++i
-    console.log(val)
+let e = 0
+let n = 0
+
+function pontoEles() {
+
+    criaEles.innerHTML = ++e
+    criaEles.classList.add('eles')
+    eles.appendChild(criaEles)
+    console.log(criaEles)
+
+    if (e >= 12) {
+
+        texto.innerHTML = 'Vencedor'
+        texto.classList.add('venci')
+        venci.appendChild(texto)
+        console.log(texto)
+
+    }
 }
 
-val.forEach((valor) => {
+function pontoNos() {
+
+    criaNos.innerHTML = ++n
+    criaNos.classList.add('nos')
+    nos.appendChild(criaNos)
+    console.log(criaNos)
+
+    if (n >= 12) {
+
+        texto.innerHTML = 'Vencedor'
+        texto.classList.add('venci')
+        titulo.appendChild(texto)
+        console.log(texto)
+
+    }
+}
+
     
-valor.addEventListener('click',adiciona)
-})
+eles.addEventListener('click', pontoEles)
+nos.addEventListener('click', pontoNos)
